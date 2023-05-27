3994dc3f12a09094e15b264fdbbfdf3657f1e6ac
Drew Jordan <ajordan12@luc.edu>
2023-05-27 11:33:26 -0500

    started js

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// housekeeping
+var citySEarchForm = document.getElementById("#city-search");
+var cityList = document.getElementById("#city-list");
+var weatherCard = document.querySelector(".card");
+var forecast = document.getElementById("forecast");
+
+// api url 
+var apiURL = "";
+
+// function 
